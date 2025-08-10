36754a3fffe2dd99d4b3371e404c353650deceeb
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-10 12:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function stringChop(str, size) {
+  if (str === null || str === undefined) return [];
+  size = Number(size);
+  if (isNaN(size) || size <= 0) return [];
+
   let ans = [];
   for (let i = 0; i < str.length; i += size) {
     ans.push(str.substring(i, i + size));
